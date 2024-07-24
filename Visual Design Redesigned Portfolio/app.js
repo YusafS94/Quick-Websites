a03bacd6f884ffb0d3cfc3769793e15eea59751d
Yusaf <42899208+YusafS94@users.noreply.github.com>
2024-07-24 23:58:56 +0100

    Add files via upload

---
+++ Visual Design Redesigned Portfolio/app.js
@@ -0,0 +1,7 @@
+//  ======================================
+//     Uncheck to make sure JS is working
+//     =====================================
+console.log("working");
+console.log("app is working");
+let element = document.getElementById("subheading");
+element.textContent = "Working JS!!!!!";
